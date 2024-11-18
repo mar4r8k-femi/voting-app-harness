321474021fc1b1073d64854f367cf652669bef5b
Olufemi Awe <mar4r8kfemi@gmail.com>
2024-11-18 00:52:46 -0500

    Update infrastructure.tf

--- aws/infrastructure.tf
+++ aws/infrastructure.tf
@@ -1,12 +1,13 @@
 resource "null_resource" "eksctl_cluster" {
   provisioner "local-exec" {
     command = <<EOT
+    curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /usr/local/bin
     eksctl create cluster \
-    --name voting-app-harness \
-    --region us-east-1 \
-    --nodegroup-name linux-nodes \
-    --node-type t2.xlarge \
-    --nodes 2
+      --name voting-app-harness \
+      --region us-east-1 \
+      --nodegroup-name linux-nodes \
+      --node-type t2.xlarge \
+      --nodes 2
     EOT
   }
 }
