dbd0e8a0f264531f8bfeb69829e6563a59f81305
Olufemi Awe <mar4r8kfemi@gmail.com>
2024-12-17 16:03:25 -0500

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "my_first_ec2_instance" {
+resource "aws_instance" "voting_app_iacm" {
   ami = "ami-0b0ea68c435eb488d" 
   instance_type = "t2.micro" # Got to https://aws.amazon.com/ec2/instance-types/t2/ for a full T2 instance type list.
   
